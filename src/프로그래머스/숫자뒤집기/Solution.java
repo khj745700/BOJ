f2ea60c29231914f33b84698c058fb24dade5202
KimHyunJin <khj745700@naver.com>
2023-06-19 19:09:42 +0900

    프로그래머스 자연수 뒤집기

---
+++ src/프로그래머스/숫자뒤집기/Solution.java
@@ -0,0 +1,17 @@
+package 프로그래머스.숫자뒤집기;
+
+public class Solution {
+    public int[] solution(long n) {
+        String str = Long.toString(n);
+        String reversed = new StringBuilder(str).reverse().toString();
+        char[] arr = reversed.toCharArray();
+
+        int[] result = new int[arr.length];
+        for (int i = 0; i < result.length; i++){
+            result[i] = arr[i] - '0';
+        }
+
+
+        return result;
+    }
+}
