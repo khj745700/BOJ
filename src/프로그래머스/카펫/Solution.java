8f92cca5286d614250c754dc01d09a156de71b44
KimHyunJin <khj745700@naver.com>
2023-06-22 12:01:47 +0900

    프로그래머스 카펫

---
+++ src/프로그래머스/카펫/Solution.java
@@ -0,0 +1,17 @@
+package 프로그래머스.카펫;
+
+public class Solution {
+    public int[] solution(int brown, int yellow) {
+        for (int width = 3; width <= 5000; width++){
+            for(int height = 3; height <= width; height++){
+                int boundary = ( width + height - 2) * 2;
+                int center = width * height - boundary;
+
+                if(brown == boundary && yellow == center) {
+                    return new int[] {width, height};
+                }
+            }
+        }
+        return null;
+    }
+}
