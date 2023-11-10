4f4f18eb4c77b37adcbeec5d30743fbfac9fa087
KimHyunJin <68643347+khj745700@users.noreply.github.com>
2023-11-10 21:19:34 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1256 ms, Memory: 304192 KB -BaekjoonHub

---
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+ 
+    static int n;
+    static int[] arr;
+    static int count = 0;
+    static boolean[] visited;
+    static boolean[] finished;
+ 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+ 
+        int tc = Integer.parseInt(br.readLine());
+ 
+        for(int t=0; t<tc; t++) {
+            n = Integer.parseInt(br.readLine());
+            arr = new int[n+1];
+            visited = new boolean[n+1];
+            finished = new boolean[n+1];
+            count = 0;
+ 
+            st = new StringTokenizer(br.readLine());
+            for(int i=1; i<n+1; i++) 
+                arr[i] = Integer.parseInt(st.nextToken());
+ 
+            for(int i=1; i<n+1; i++)
+                dfs(i);
+ 
+            System.out.println(n - count);
+        }
+    }
+ 
+    static void dfs(int now) { 
+        if(visited[now])
+            return;
+ 
+        visited[now] = true;
+        int next = arr[now];
+ 
+        if(visited[next] != true)
+            dfs(next);
+        else {
+            if(finished[next] != true) {
+                count++;
+                for(int i=next; i != now; i = arr[i])
+                    count++;
+            }
+        }
+        finished[now] = true;
+    }
+}
