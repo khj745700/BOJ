913baf81ffc1682a8600d32f42b98eae26c243a4
KimHyunJin <68643347+khj745700@users.noreply.github.com>
2024-01-17 00:22:38 +0900

    [Silver III] Title: N과 M (5), Time: 432 ms, Memory: 47836 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,47 +1,52 @@
-
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
-
 public class Main {
-    static int[] data;
-    static int[] ans;
-    static int[] visit;
+    static int N;
+    static int depth;
 
-    static void pers(int depth, int N, int R){
-        if (depth == R){
-            //완성,, 출력하기
-            for(int i = 0 ; i < R ; i++){
-                System.out.print(ans[i]+" ");
+    static int[] arr;
+    static boolean[] visited;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        depth = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        visited = new boolean[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        backTracking(0, new ArrayList<>());
+        bw.flush();
+    }
+
+
+    static void backTracking(int depth, List<Integer> list) throws IOException {
+        if(depth == Main.depth) {
+            for(int i = 0; i < list.size(); i++) {
+                bw.append(list.get(i).toString()).append(" ");
             }
-            System.out.println();
+            bw.newLine();
             return;
         }
-        for(int i = 0 ; i < N; i++){
-            if(visit[i] == 0){
-                ans[depth] = data[i];
-                visit[i] = 1;
-                pers(depth+1, N, R);
-                visit[i] = 0;
+
+        for(int i = 0; i < N; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                list.add(arr[i]);
+                backTracking(depth + 1, list);
+                list.remove((Integer) arr[i]);
+                visited[i] = false;
             }
         }
-
-    }
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        data = new int[n];
-        ans = new int[n];
-        visit = new int[n];
-        for(int i = 0; i < n; i++){
-            data[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(data);
-        pers(0,n,m);
     }
 }
