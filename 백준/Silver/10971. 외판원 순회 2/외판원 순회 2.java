73e09ea8ab09fd8c110a021500ee68bb399f2c81
KimHyunJin <68643347+khj745700@users.noreply.github.com>
2024-01-17 19:21:40 +0900

    [Silver II] Title: 외판원 순회 2, Time: 660 ms, Memory: 177760 KB -BaekjoonHub

---
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    static boolean[] visited;
+    static int[][] map;
+    static int N;
+    static int min = Integer.MAX_VALUE;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        map = new int[N][N];
+        visited = new boolean[N];
+        StringTokenizer st;
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+
+        for(int i = 0 ; i < N; i++) {
+            List<Integer> list = new ArrayList<>();
+            backtracking(i, 0, i, list);
+        }
+        System.out.println(min);
+    }
+
+
+    static void backtracking(int start, int depth, int now, List<Integer> list) {
+        if(depth == N - 1 && map[now][start] != 0) {
+            int sum = 0;
+            for(Integer i : list) {
+                sum += i;
+            }
+            sum += map[now][start];
+            min = Math.min(sum, min);
+            return;
+        }
+
+        visited[now] = true;
+        for(int i = 0; i < N; i++) {
+            if(visited[i] || map[now][i] == 0) {
+                continue;
+            }
+            list.add(map[now][i]);
+            backtracking(start, depth + 1, i, list);
+            list.remove(list.size() -1);
+        }
+        visited[now] = false;
+    }
+}
