aa01bdfb8cd5311909c5354a89203ffac8a406dc
KimHyunJin <68643347+khj745700@users.noreply.github.com>
2024-01-26 16:51:19 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 616 ms, Memory: 130172 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,65 +1,64 @@
-import java.io.*;
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 public class Main{
-    static int N, M;
-    static int[][] graph;
-    static int count;
-    static boolean[] visit;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        graph = new int[N][N];
-        visit = new boolean[N];
-        for(int i = 0 ; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int u, v;
-            u = Integer.parseInt(st.nextToken()) - 1;
-            v = Integer.parseInt(st.nextToken()) - 1;
-            graph[u][v] = 1;
-            graph[v][u] = 1;
-        }
-        int startNode;
-
-        while((startNode = findFirstIndex()) != -1) {
-            dfs(startNode);
-            count++;
-        }
-        for(int i =0 ; i < visit.length; i++) {
-            if(!visit[i]){
-                count++;
-            }
-        }
-        System.out.println(count);
-    }
-
-    static void dfs(int startNode) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(startNode);
-
-        while(!stack.isEmpty()) {
-            int node = stack.pop();
-            visit[node] = true;
-            for(int i = 0; i < N; i++) {
-                if(graph[node][i] == 1 && !visit[i]) {
-                    stack.push(i);
-                }
-            }
-        }
-    }
-
-    static int findFirstIndex() {
-        for(int i = 0 ; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                if(graph[i][j] == 1 && !visit[i]) {
-                    return i;
-                }
-            }
-        }
-        return -1;
-    }
+	static int[][] map;
+	static boolean[] visited;
+	static int N, M;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		map = new int[N][N];
+		visited = new boolean[N];
+		
+		for(int i = 0 ; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken()) - 1;
+			int v = Integer.parseInt(st.nextToken()) - 1;
+			map[u][v] = 1;
+			map[v][u] = 1;
+		}
+		
+		int count = 0;
+		for(int i = 0; i < N; i++) {
+			if(!visited[i]) {
+				bfs(i);
+				count++;
+			}
+		}
+		
+		System.out.println(count);
+	}
+	
+	
+	
+	static void bfs(int startIdx) {
+		Queue<Integer> q = new ArrayDeque<>();
+		q.add(startIdx);
+		
+		while(!q.isEmpty()) {
+			int cur = q.poll();
+			if(visited[cur]) {
+				continue;
+			}
+			visited[cur] = true;
+			for(int i = 0; i < N; i++) {
+				if(!visited[i] && map[cur][i] > 0) {
+					q.add(i);
+				}
+			}
+		}
+	}
 }
