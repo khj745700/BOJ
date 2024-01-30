d832aca8cda259010a66c8be32fdbadbe88f4ba6
KimHyunJin <68643347+khj745700@users.noreply.github.com>
2024-01-30 13:34:05 +0900

    [Gold IV] Title: 이분 그래프, Time: 1104 ms, Memory: 304232 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -1,77 +1,62 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
-    public static ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
-    public static int[] visited;
-    public static int V;
-    public static int E;
-    public static String res;
+	
+	public static void solution(List<Integer>[] lists, int v, int e) {
+		Boolean[] value_matrix = new Boolean[v];
+		Queue<Integer> queue = new ArrayDeque<>();
+		for(int i=0; i<v; i++) {
+			if(value_matrix[i] == null) {
+				value_matrix[i] = true;
+				queue.add(i);
+				while(!queue.isEmpty()) {
+					int vertex = queue.poll();
+					for(int j : lists[vertex])
+						if(value_matrix[j] == null) {
+							queue.add(j);
+							value_matrix[j] = !value_matrix[vertex];
+						}
+						else
+							if(value_matrix[j] == value_matrix[vertex]) {
+								System.out.println("NO");
+								return;
+							}
+				}
+			}
+		}
+		System.out.println("YES");
+	}
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		int[] v = new int[t];
+		int[] e = new int[t];
+		
+		List<Integer>[] list = null;
+		
+		for(int i=0; i<t; i++) {
+			String[] temp = br.readLine().split(" ");
+			v[i] = Integer.parseInt(temp[0]);
+			e[i] = Integer.parseInt(temp[1]);
+			list = new ArrayList[v[i]];
+			for(int j=0; j<v[i]; j++)
+				list[j] = new ArrayList<>();
+			for(int j=0; j<e[i]; j++) {
+				temp = br.readLine().split(" ");
+				int x = Integer.parseInt(temp[0])-1;
+				int y = Integer.parseInt(temp[1])-1;
+				list[x].add(y);
+				list[y].add(x);
+			}
+			solution(list, v[i], e[i]);
 
-    public static void bfs(int s) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(s);
-        visited[s] = 1; //임의로 하는게 맞나,,
-
-        while(!q.isEmpty()) {
-            int x = q.poll();
-            for(int i=0;i<arr.get(x).size();i++) {
-                if(visited[arr.get(x).get(i)]==0) {
-                    int nc = visited[x]==1? 2:1;
-                    visited[arr.get(x).get(i)] = nc;
-                    q.add(arr.get(x).get(i));
-                }
-                else {
-                    if(visited[arr.get(x).get(i)]==visited[x]) {
-                        res = "NO";
-                        return;
-                    }
-                }
-            }
-        }
-    }
-
-public static void main(String[] args) throws NumberFormatException, IOException {
-    // TODO Auto-generated method stub
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st;
-    
-    int T = Integer.parseInt(br.readLine());
-    
-    for(int t = 0;t<T ; t++) {
-        st = new StringTokenizer(br.readLine());
-        V = Integer.parseInt(st.nextToken()); // 정점 수 
-        E = Integer.parseInt(st.nextToken()); // 간선 수 
-        visited = new int[V+1];
-        res = "YES";
-        arr = new ArrayList<>();
-        for(int i=0;i<V+1 ; i++) {
-            arr.add(new ArrayList<Integer>());
-        }
-        
-        for(int i=0;i<E ;i++) {
-            st= new StringTokenizer(br.readLine());
-            
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            
-            arr.get(a).add(b);
-            arr.get(b).add(a);
-        }
-        
-        for(int i=1; i<V+1 ; i++) {
-            if(visited[i]==0) {
-                bfs(i);
-            }
-        }
-        System.out.println(res);
-    }
-    
-}
+		}
+	}
 }
