2171b4a8711a274046e96bd8aea21044f8430097
KimHyunJin <68643347+khj745700@users.noreply.github.com>
2024-01-30 10:22:11 +0900

    [Gold IV] Title: 이분 그래프, Time: 1220 ms, Memory: 279760 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -1,85 +1,77 @@
-import java.io.*;
-import java.util.*;
-public class Main{
-    static int K, V, E;
-    static List<List<Integer>> graph;
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int[] visit;
 
-    static final int NOT_VISIT = 0;
-    static final int RED = -1;
-    static final int BLACK = 1;
-    public static void main(String[] args) throws IOException {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-        K = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        while(K --> 0) {
-            st = new StringTokenizer(br.readLine());
-            V = Integer.parseInt(st.nextToken());
-            E = Integer.parseInt(st.nextToken());
-            visit = new int[V];
+public class Main {
+    public static ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+    public static int[] visited;
+    public static int V;
+    public static int E;
+    public static String res;
 
-            setGraph();
-            boolean isBiGraph = false;
-            for(int i = 0 ; i < V; i++) {
-                if(visit[i] == NOT_VISIT){
-                    isBiGraph = BFS(i);
+    public static void bfs(int s) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(s);
+        visited[s] = 1; //임의로 하는게 맞나,,
+
+        while(!q.isEmpty()) {
+            int x = q.poll();
+            for(int i=0;i<arr.get(x).size();i++) {
+                if(visited[arr.get(x).get(i)]==0) {
+                    int nc = visited[x]==1? 2:1;
+                    visited[arr.get(x).get(i)] = nc;
+                    q.add(arr.get(x).get(i));
                 }
-
-                if(!isBiGraph) {
-                    break;
-                }
-            }
-            if(isBiGraph) {
-                bw.append("YES");
-            }else {
-                bw.append("NO");
-            }
-            bw.newLine();
-        }
-        bw.flush();
-    }
-
-
-    static void setGraph() throws IOException {
-        StringTokenizer st;
-        graph = new ArrayList<>();
-        for (int i = 0 ; i < V; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for (int i = 0 ; i < E ; i++) {
-            st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken()) - 1;
-            int to = Integer.parseInt(st.nextToken()) - 1;
-
-            graph.get(from).add(to);
-            graph.get(to).add(from);
-
-        }
-    }
-
-    static boolean BFS(int start) {
-        Queue<Integer> q = new ArrayDeque<>();
-        q.add(start);
-
-        visit[start] = RED;
-
-        while(!q.isEmpty()){
-            int cur = q.poll();
-            for(int i : graph.get(cur)) {
-                if(visit[cur] == visit[i]) {
-                    return false;
-                }
-
-                if(visit[i] == NOT_VISIT){
-                    q.add(i);
-                    visit[i] = visit[cur] * -1;
+                else {
+                    if(visited[arr.get(x).get(i)]==visited[x]) {
+                        res = "NO";
+                        return;
+                    }
                 }
             }
         }
-
-        return true;
     }
+
+public static void main(String[] args) throws NumberFormatException, IOException {
+    // TODO Auto-generated method stub
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    
+    int T = Integer.parseInt(br.readLine());
+    
+    for(int t = 0;t<T ; t++) {
+        st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken()); // 정점 수 
+        E = Integer.parseInt(st.nextToken()); // 간선 수 
+        visited = new int[V+1];
+        res = "YES";
+        arr = new ArrayList<>();
+        for(int i=0;i<V+1 ; i++) {
+            arr.add(new ArrayList<Integer>());
+        }
+        
+        for(int i=0;i<E ;i++) {
+            st= new StringTokenizer(br.readLine());
+            
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            arr.get(a).add(b);
+            arr.get(b).add(a);
+        }
+        
+        for(int i=1; i<V+1 ; i++) {
+            if(visited[i]==0) {
+                bfs(i);
+            }
+        }
+        System.out.println(res);
+    }
+    
+}
 }
