84dabee076324b65b069ccfaa1f48e1282637aab
KimHyunJin <68643347+khj745700@users.noreply.github.com>
2024-02-02 21:31:42 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1492 ms, Memory: 74640 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static List<Integer>[] arr;
+    static int[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new List[N+1];
+        visited = new int[N+1];
+        for(int i = 0; i <= N; i++) {
+            arr[i] = new ArrayList<>();
+        }
+
+        for(int i = 0 ; i < N - 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            arr[u].add(v);
+            arr[v].add(u);
+        }
+        visited[1] = 1;
+        buildTrees(1);
+        for(int i = 2; i <= N; i++) {
+            System.out.println(visited[i]);
+        }
+    }
+
+    static void buildTrees(int start) {
+        for(Integer v : arr[start]) {
+            if(visited[v] > 0) {
+                continue;
+            }
+            visited[v] = start;
+           buildTrees(v);
+        }
+    }
+
+}
