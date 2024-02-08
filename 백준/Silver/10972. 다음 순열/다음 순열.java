b67e4ec163c144b1e7df14a57a72d311d6c8ab21
KimHyunJin <68643347+khj745700@users.noreply.github.com>
2024-02-08 11:31:09 +0900

    [Silver III] Title: 다음 순열, Time: 176 ms, Memory: 15544 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+ 
+public class Main {
+	static int N, M;
+	static int[] arr;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		boolean flag = np();
+		if(flag) {
+			for(int i : arr) {
+				sb.append(i);
+				sb.append(' ');
+			}
+		}else {
+			sb.append(-1);
+		}
+		System.out.println(sb);
+	}
+	
+	
+	
+	
+	
+	static boolean np() {
+		//find top;
+		int i = N-1;
+		while(i > 0 && arr[i] < arr[i-1]) i--;
+		
+		if(i == 0) {
+			return false;
+		}
+		
+		// find top까지 i-1 보다 큰 가장 최솟값.
+		int K = N-1;
+		while(arr[K]<arr[i-1]) K--;
+		swap(i-1,K);
+		
+		
+		int j = N-1;
+		while(i < j) swap(i++, j--);
+		
+		return true;
+	}
+	
+	
+	static void swap(int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+}
