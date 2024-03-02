90b1949a0bb78f553bef3b45cf732401d01b7a33
KimHyunJin <68643347+khj745700@users.noreply.github.com>
2024-03-02 18:49:00 +0900

    [Gold IV] Title: 게리맨더링, Time: 140 ms, Memory: 16468 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -1,144 +1,119 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Queue;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	static int N;
-	static List<Integer>[] map;
-	static int[] arr;
-	
-	static boolean[] check;
-	static int MIN = Integer.MAX_VALUE;
-	static int total = 0;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		
-		map = new List[N+1];
-		arr = new int[N+1];
-		
-		for(int i = 1; i <= N; i++) {
-			map[i] = new ArrayList<>();
-		}
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 1; i <= N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			total+= arr[i];
-		}
+    static int N;
+    static List<Integer>[] map;
+    static int[] population;
+    static boolean[] check;
+    static int MIN = Integer.MAX_VALUE;
+    static int total = 0;
+    static final boolean RED = true;
+    static final boolean BLUE = false;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
 
-		check = new boolean[N+1];
-		for(int i = 1 ; i <= N; i++) {
-			st = new StringTokenizer(br.readLine());
-			int size = Integer.parseInt(st.nextToken());
-			while(st.hasMoreTokens()) {
-				int v = Integer.parseInt(st.nextToken());
-				map[i].add(v);
-			}
-		}
-		
-		pomutation(1, new ArrayList<>());
-		System.out.println(MIN == Integer.MAX_VALUE ? -1 : MIN);
-	}
-	
-	
-	static void pomutation(int depth, List<Integer> l) {
-		boolean flag = true;
-		boolean flag2 = true;
-		if(depth <= N) {
-			//한 녀석이 인접하고 다른 녀석도 인접한지 확인해야 함. 한 녀석만 인접하면 됨.
-				for(int j = 1; j <= N; j++) {
-					if(!check[j]) {
-						continue;
-					}
-					if(!bfs(l.get(0),j, l)) {
-						flag = false;
-						break;
-					}
-				}
-			
-			if(flag) {
-				List<Integer> r = new ArrayList<>();
-				for(int i = 1; i <= N; i++) {
-					if(!check[i]) {
-						r.add(i);
-					}
-				}
-				for(int j = 1; j <= N; j++ ) {
-					if(check[j]) {
-						continue;
-					}
-					if(!bfs(r.get(0),j,r)) {
-						flag2 = false;
-						break;
-					}
-				}
-				
-				if(flag2) {
-					int l1 = 0;
-					for(int i = 1; i <= N; i++) {
-						if(check[i]) {
-							l1+= arr[i];
-						}
-					}
-					int l2 = total - l1;
-					
-					if(l1 > l2 && l1 - l2 > MIN) {
-						return;
-					}
+        map = new List[N+1];
+        population = new int[N+1];
 
-					MIN = Math.min(Math.abs(l1 - l2), MIN);
-				}
-			}
-		}
-		for(int i = depth; i <= N; i++) {
-			if(check[i]) {
-				continue;
-			}
-			check[i] = true;
-			l.add(i);
-			pomutation(i+1, l);
-			l.remove(l.size() - 1);
-			check[i] = false;
-		}
-	}
-	
-	
-	
-	
-	static boolean bfs(int u, int v, List<Integer> list) {
-		Queue<Integer> q = new ArrayDeque<>();
-		q.add(u);
+        for(int i = 0; i <= N; i++) {
+            map[i] = new ArrayList<>();
+        }
 
-		boolean[] visited = new boolean[N+1];
-		
-		while(!q.isEmpty()) {
-			int cur = q.poll();
-			
-			if(visited[cur]) {
-				continue;
-			}
-			
-			if(cur == v) {
-				return true;
-			}
-			
-			visited[cur] = true;
-			
-			for(int vortex : map[cur]) {
-				if(!visited[vortex] && list.contains(vortex))
-					q.add(vortex);
-				}
-			}
-		//자식 트리 탐색
-		return false;
-	}
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= N; i++) {
+            population[i] = Integer.parseInt(st.nextToken());
+            total+= population[i];
+        }
+
+        check = new boolean[N+1];
+        for(int i = 1 ; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int size = Integer.parseInt(st.nextToken());
+            while(size --> 0) {
+                int v = Integer.parseInt(st.nextToken());
+                map[i].add(v);
+            }
+        }
+        permutation(1);
+        System.out.println(MIN == Integer.MAX_VALUE ? -1 : MIN);
+    }
+    static void permutation(int depth) {
+        if(depth <= N) {
+            if(isAllConnection(getStartIdx(RED), RED) && isAllConnection(getStartIdx(BLUE), BLUE)){
+                int redPopulation = getPopularSum(RED);
+                int bluePopulation = total - redPopulation;
+                MIN = Math.min(Math.abs(redPopulation - bluePopulation), MIN);
+            }
+        }
+
+        for(int i = depth; i <= N; i++) {
+            if(check[i] == RED) {
+                continue;
+            }
+            check[i] = RED;
+            permutation(i+1);
+            check[i] = BLUE;
+        }
+    }
+
+    static boolean connectionCheck(int u, int v, boolean flag) {
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(u);
+        boolean[] visited = new boolean[N+1];
+        while(!q.isEmpty()) {
+
+            int cur = q.poll();
+            if(visited[cur]) {
+                continue;
+            }
+
+            if(cur == v) {
+                return true;
+            }
+            visited[cur] = true;
+            for(int vortex : map[cur]) {
+                if(!visited[vortex] && check[vortex] == flag){
+                    q.add(vortex);
+                }
+            }
+        }
+        return false;
+    }
+
+
+    static int getStartIdx(boolean flag) {
+        for(int i = 1; i <= N; i++) {
+            if(check[i] == flag) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    static boolean isAllConnection(int start, boolean flag) {
+        for(int j = 1; j <= N; j++ ) {
+            if (check[j] != flag) {
+                continue;
+            }
+
+            if (!connectionCheck(start, j, flag)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static int getPopularSum(boolean flag) {
+        int ans = 0;
+        for(int i = 1; i <= N; i++) {
+            if(check[i] == flag) {
+                ans+= population[i];
+            }
+        }
+        return ans;
+    }
 }
