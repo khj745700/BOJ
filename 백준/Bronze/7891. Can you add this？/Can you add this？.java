a47e6b2ec0dba1c09dca4e099718edc7e9e59013
KimHyunJin <68643347+khj745700@users.noreply.github.com>
2024-03-15 17:30:47 +0900

    [Bronze V] Title: Can you add this?, Time: 76 ms, Memory: 11608 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		
+		while(T --> 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			sb.append(x + y);
+			sb.append('\n');
+		}
+		System.out.println(sb);
+	}
+}
