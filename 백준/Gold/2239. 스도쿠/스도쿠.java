fe673b8c42ed6ac1534d38255eb43127b1e1a41e
KimHyunJin <68643347+khj745700@users.noreply.github.com>
2024-03-27 13:40:09 +0900

    [Gold IV] Title: 스도쿠, Time: 572 ms, Memory: 23880 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -1,88 +1,102 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class Main {
-    static int[][] arr;
-    static int total;
-    static boolean[][] visited;
-    public static void main(String[] args) throws IOException {
-        arr = new int[9][9];
-        visited = new boolean[9][9];
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i< 9; i++) {
-            String input = br.readLine();
-            for(int j = 0 ; j < 9; j++) {
-                arr[i][j] = input.charAt(j) - '0';
-                if(arr[i][j] == 0) {
-                    total++;
-                    visited[i][j] = true;
-                }
-            }
-        }
-        backtracking(0,0);
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0 ; j < 9; j++) {
-                sb.append(arr[i][j]);
-            }
-            sb.append('\n');
-        }
-        System.out.println(sb);
-    }
-
-
-    static boolean backtracking(int depth, int cnt) {
-        if(depth == 81) {
-            return true;
-        }
-
-            int x = depth % 9;
-            int y =  depth / 9;
-            if(!visited[y][x]) {
-                return backtracking(depth+1, cnt);
-            }
-            for(int j = 1 ; j <= 9; j++) {
-                if(h(x,y,j) && v(x,y,j) && square(x,y,j)){
-                    arr[y][x] = j;
-                    if(backtracking(depth+1, cnt+1)) {
-                        return true;
-                    }
-                    arr[y][x] = 0;
-                }
-            }
-        return false;
-    }
-
-
-
-    static boolean h(int x, int y, int val) {
-        for(int i = 0; i < 9; i++) {
-            if(arr[y][i] == val) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    static boolean v(int x, int y, int val) {
-        for(int i = 0; i < 9; i++) {
-            if(arr[i][x] == val) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    static boolean square(int x, int y, int val) {
-        for(int i = (x/3)*3 ; i < (x/3)*3+3; i++) {
-            for(int j = (y/3)*3; j < (y/3)*3+3; j++){
-                if(arr[j][i] == val) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+	static int[][] arr;
+	static List<Integer>[][] possible;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		possible = new List[9][9];
+		arr = new int[9][9];
+		for(int i = 0 ; i < 9; i++) {
+			for(int j = 0 ; j < 9; j++) {
+				possible[i][j] = new ArrayList<>(9);
+			}
+		}
+		for(int i = 0 ; i < 9; i++) {
+			String input = br.readLine();
+			for(int j = 0 ; j < 9; j++) {
+				arr[i][j] = input.charAt(j)-'0';
+			}
+		}
+		
+		for(int i = 0 ; i < 9; i++) {
+			for(int j = 0 ; j < 9; j++) {
+				for(int k = 1; k < 10; k++) {
+					if(isRowPossible(j, i, k) && isColumnPossible(j, i, k) && isSquarePossible(j, i, k)) {
+						possible[i][j].add(k);
+					}
+				}
+			}
+		}
+		
+		bt(0);
+		for(int i = 0 ; i < 9; i++) {
+			for(int j = 0 ; j < 9; j++) {
+				sb.append(arr[i][j]);
+			}
+			sb.append('\n');
+		}
+		System.out.println(sb);
+	}
+	
+	static boolean bt(int depth) {
+		if(depth == 81) {
+			return true;
+		}
+		
+		int x = depth % 9;
+		int y = depth / 9;
+		
+		if(arr[y][x] != 0) {
+			return bt(depth+1);
+		}
+		
+		for(int i : possible[y][x]) {
+			if(isRowPossible(x, y, i) && isColumnPossible(x, y, i) && isSquarePossible(x, y, i)) {
+				arr[y][x] = i;
+				if(bt(depth+1)) {
+					return true;
+				}
+				arr[y][x] = 0;
+			}
+		}
+		
+		return false;
+	}
+	
+	static boolean isRowPossible(int x, int y, int v) {
+		for(int i = 0; i < 9; i++) {
+			if(arr[y][i] == v) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	static boolean isColumnPossible(int x, int y, int v) {
+		for(int i = 0; i < 9; i++) {
+			if(arr[i][x] == v) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	static boolean isSquarePossible(int x, int y, int v) {
+		for(int i = y/3 * 3 ; i < (y/3 * 3) +3; i++) {
+			for(int j = x/3 * 3 ; j < (x/3 * 3) + 3; j++) {
+				if(arr[i][j] == v) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 }
