13492ea6da0fdd7ffc22a158bcd78405f09ef9ee
KimHyunJin <68643347+khj745700@users.noreply.github.com>
2024-04-05 17:07:10 +0900

    [Silver I] Title: 나무 탈출, Time: 1640 ms, Memory: 259108 KB -BaekjoonHub

--- 백준/Silver/15900. 나무 탈출/나무 탈출.java
+++ 백준/Silver/15900. 나무 탈출/나무 탈출.java
@@ -10,33 +10,31 @@ public class Main {
 	static Node root;
 	static int N;
 	static int depth = 0;
-	static boolean[] visited;
     public static void main(String[] args) throws Exception{
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	N = Integer.parseInt(br.readLine());
     	root = new Node(1);
     	bucket = new HashMap<>();
     	bucket.put(1, root);
-    	visited = new boolean[N+1];
+    	for(int i = 2; i <= N; i++) {
+    		bucket.put(i, new Node(i));
+    	}
     	for(int i = 0 ; i < N-1; i++) {
     		StringTokenizer st = new StringTokenizer(br.readLine());
     		int v1 = Integer.parseInt(st.nextToken());
     		int v2 = Integer.parseInt(st.nextToken());
-    		bucket.putIfAbsent(v1, new Node(v1));
-    		bucket.putIfAbsent(v2, new Node(v2));
     		Node v = bucket.get(v1);
     		Node u = bucket.get(v2);
     		v.childs.add(u);
     		u.childs.add(v);
     	}
     	
-    	dfsCount(root, 0);
+    	dfsCount(null, root, 0);
     	System.out.println(depth % 2 == 0 ? "No" : "Yes");
     }
     
     
-    static void dfsCount(Node cur, int count) {
-    	visited[cur.val] = true;
+    static void dfsCount(Node parent, Node cur, int count) {
     	
     	if(cur.childs.size() == 1 && cur != root) {
     		depth+= count;
@@ -44,10 +42,10 @@ public class Main {
     	}
     	
     	for(Node v : cur.childs) {
-    		if(visited[v.val]) {
+    		if(parent == v) {
     			continue;
     		}
-    		dfsCount(v, count+1);
+    		dfsCount(cur, v, count+1);
     	}
     	
     }
