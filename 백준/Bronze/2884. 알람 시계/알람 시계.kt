119be311df7ce42c5733316abc30da531e79443e
KimHyunJin <68643347+khj745700@users.noreply.github.com>
2025-02-28 13:21:37 +0900

    [Bronze III] Title: 알람 시계, Time: 80 ms, Memory: 12568 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.kt
@@ -0,0 +1,16 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.time.LocalTime
+import java.util.*
+
+fun main() {
+    var br = BufferedReader(InputStreamReader(System.`in`))
+    var st  = StringTokenizer(br.readLine())
+    var h = st.nextToken().toInt()
+    var m = st.nextToken().toInt()
+
+    var time = LocalTime.of(h, m)
+
+    time = time.minusMinutes(45);
+    print(time.hour.toString() + " " + time.minute.toString())
+}
